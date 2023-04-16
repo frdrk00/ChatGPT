61a71014e078cb37e5d2ec35851e53b29dfed876
frdrk00 <masterkooo@gmail.com>
2023-04-16 12:02:37 +0200

    Connecting to the Cloud Firebase Database & Building ChatRow Component

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,12 +1,23 @@
 "use client";
 
+import { useCollection } from "react-firebase-hooks/firestore";
+import { collection } from "firebase/firestore";
+import { db } from "@/firebase";
 import { signOut, useSession } from "next-auth/react";
 import NewChat from "./NewChat";
 import Image from "next/image";
+import ChatRow from "./ChatRow";
+
 
 const SideBar = () => {
   const { data: session } = useSession();
 
+  const [chats, loading, error] = useCollection(
+    session && collection(db, 'users', session.user?.email!, 'chats')
+  )
+
+  console.log(chats);
+  
   return (
     <div className="p-2 flex flex-col h-screen">
       <div className="flex-1">
@@ -16,6 +27,9 @@ const SideBar = () => {
           <div>{/* ModelSelection */}</div>
 
           {/* Map through there ChatTows */}
+          {chats?.docs.map(chat => (
+            <ChatRow key={chat.id} id={chat.id} />
+          ))}
         </div>
       </div>
       {session && (
