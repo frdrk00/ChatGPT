16be5a110b3dbe018d44112a25fd0dd0bb753b40
frdrk00 <masterkooo@gmail.com>
2023-04-16 15:28:20 +0200

    Setting up OpenAI API Key

---
+++ pages/api/auth/askQuestion.ts
@@ -0,0 +1,35 @@
+import { query } from 'firebase/firestore'
+import type { 
+    NextApiRequest,
+    NextApiResponse
+} from 'next'
+
+type Data = {
+    name: string
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+        const {prompt, chatId, model, session } = req.body
+
+        if (!prompt) {
+            res.status(400).json({ answer: 'Please provide a prompt!' })
+            return
+        }
+
+        if (!chatId) {
+            res.status(400).json({ answer: 'Please provide a valid chat ID!' })
+            return
+        }
+
+        // ChatGPT Query
+        const response = await query(prompt, chatId, model)
+
+        const message: Message = {
+            text: response || "ChatGPT was unable to find an answer for that!",
+            //
+        }
+    res.status(200).json({ name: 'John Doe' })
+}
